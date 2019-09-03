d01ce4dcf76cea97a472889fd5b58c3e32ac02e5
Stenia Felix <steniafelix@gmail.com>
2019-09-03 02:08:24 -0300

    Configuring user update

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -10,6 +10,32 @@ class UserController {
       email
     });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email != user.email) {
+      const userExist = await User.findOne({ where: { email } });
+
+      if (userExist) {
+        return res.status(400).json({ error: "User already exists." });
+      }
+    }
+
+    if (oldpassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: "Password does not match" });
+    }
+
+    const { id, name } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email
+    });
+  }
 }
 
 export default new UserController();
