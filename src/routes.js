9b294b511bd7b2f2213049ca63b3521a4c1fb13f
Stenia Felix <steniafelix@gmail.com>
2019-09-13 17:09:24 -0300

    Adding route to upload file

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,12 @@ import multerConfig from "./config/multer";
 
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
+import FileController from "./app/controllers/FileController";
 
 import authMiddleware from "./app/middlewares/auth";
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post("/users", UserController.store);
 routes.post("/sessions", SessionController.store);
@@ -15,5 +17,7 @@ routes.post("/sessions", SessionController.store);
 routes.use(authMiddleware);
 
 routes.put("/users", UserController.update);
+
+routes.post("/files", upload.single("file"), FileController.store);
 
 export default routes;
