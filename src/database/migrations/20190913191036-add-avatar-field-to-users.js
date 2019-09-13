07ad6a1074ad78bd2629d95355ea8c6c074ea575
Stenia Felix <steniafelix@gmail.com>
2019-09-13 16:31:31 -0300

    Migration to adding column avatar_id in users tables

--- src/database/migrations/20190913191036-add-avatar-field-to-users.js
+++ src/database/migrations/20190913191036-add-avatar-field-to-users.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColunm("users", "avatar_id", {
+    return queryInterface.addColumn("users", "avatar_id", {
       type: Sequelize.INTEGER,
       references: { model: "files", key: "id" },
       onUpdate: "CASCADE",
@@ -12,6 +12,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.removeColunm("users", "vatar_id");
+    return queryInterface.removeColumn("users", "vatar_id");
   }
 };
