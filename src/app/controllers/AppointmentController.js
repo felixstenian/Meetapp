d107a5f5adb432b37f574c087093b66cd6e7f430
Stenia Felix <steniafelix@gmail.com>
2019-09-17 16:17:35 -0300

    Creating new meetaap

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,30 @@
 import Appointment from "../models/Appointment";
+import * as Yup from "yup";
 
 class AppointmentController {
   async store(req, res) {
-    return res.json();
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      file_id: Yup.number().required(),
+      description: Yup.string().required(),
+      location: Yup.string().required(),
+      date: Yup.date().required()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "Validation fails" });
+    }
+
+    const { title, description, location, date, file_id } = req.body;
+
+    const user_id = req.userId;
+
+    const appointment = await Appointment.create({
+      ...req.body,
+      user_id
+    });
+
+    return res.json(appointment);
   }
 }
 
