e1454631a36913082113c602ec848f6f17f019df
Stenia Felix <steniafelix@gmail.com>
2019-09-17 23:52:59 -0300

    Corrections

--- src/database/index.js
+++ src/database/index.js
@@ -2,9 +2,9 @@ import Sequelize from "sequelize";
 
 import User from "../app/models/User";
 import File from "../app/models/File";
+import Appointment from "../app/models/Appointment";
 
 import databaseConfig from "../config/database";
-import Appointment from "../app/models/Appointment";
 
 const models = [User, File, Appointment];
 
