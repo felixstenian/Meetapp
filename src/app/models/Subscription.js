3d2ada483416be7b8e0d67b7d2f8a2f3326f46e6
Stenia Felix <steniafelix@gmail.com>
2019-09-23 00:11:21 -0300

    Corrections

--- src/app/models/Subscription.js
+++ src/app/models/Subscription.js
@@ -1,10 +1,11 @@
-import { Model } from "sequelize";
-
 class Subscription extends Model {
   static init(sequelize) {
-    super.init({
-      sequelize
-    });
+    super.init(
+      {},
+      {
+        sequelize
+      }
+    );
 
     return this;
   }
