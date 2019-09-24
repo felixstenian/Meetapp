5a26e632bc56f8cbd7fff5cb26b5cf43982519ee
Stenia Felix <steniafelix@gmail.com>
2019-09-24 01:27:27 -0300

    Corrections in associate models

--- src/app/models/Subscription.js
+++ src/app/models/Subscription.js
@@ -11,11 +11,10 @@ class Subscription extends Model {
   }
 
   static associate(models) {
-    this.belongsTo(models.User, { foreignKey: "user_id", as: "user" });
     this.belongsTo(models.Appointment, {
-      foreignKey: "appointment_id",
-      as: "appointment"
+      foreignKey: "appointment_id"
     });
+    this.belongsTo(models.User, { foreignKey: "user_id" });
   }
 }
 
