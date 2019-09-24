21213d9aa1109ccee7e7c6864e8dc1b26352628b
Stenia Felix <steniafelix@gmail.com>
2019-09-24 01:10:43 -0300

    Validation dates past

--- src/app/controllers/SubscriptionController.js
+++ src/app/controllers/SubscriptionController.js
@@ -6,14 +6,20 @@ import User from "../models/User";
 class SubscriptionController {
   async store(req, res) {
     const user = await User.findByPk(req.userID);
-    const meetapp = await Appointment.findByPk(req.params.appointmentId, {
+    const appointment = await Appointment.findByPk(req.params.appointmentId, {
       include: [User]
     });
 
-    if (meetapp.user_id === req.userId) {
+    if (appointment.user_id === req.userId) {
       return res
         .status(400)
         .json({ error: "Can't subscribe to you own meetups !" });
+    }
+
+    if (appointment.past) {
+      return res
+        .status(400)
+        .json({ error: "Can't subscribe to past meetups !" });
     }
 
     return res.json({ ok: true });
